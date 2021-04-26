c7ca628a542d2d694e6ccfd2fcc412ff98f7fca9
Vladislav Bielousov <97beloucovva@gmail.com>
2021-04-26 23:14:38 +0300

    Add js task

---
+++ JS/index.js
@@ -0,0 +1,35 @@
+window.onload = async function () {
+  const divForLetters = document.querySelector(".letters");
+  const divForResult = document.querySelector(".result");
+
+  const response = await fetch("list.json");
+  const list = await response.json();
+  
+  const letterHandler = (event) => {
+    while (divForResult.firstChild) {
+      divForResult.removeChild(divForResult.firstChild);
+    }
+    const result = list.filter(
+      (item) => item.name[0] === event.target.innerText 
+    );
+    result.map((char) => {
+      divForResult.append(char.name + ", ");
+    });
+  };
+
+  (function randomLetters() {
+    const charArr = "12345".split("").map(function () {
+      return "ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(
+        Math.floor(26 * Math.random())
+      );
+    });
+
+    charArr.map((char) => {
+      var charBlock = document.createElement("div");
+      charBlock.classList.add("letter")
+      charBlock.textContent = char;
+      charBlock.addEventListener('click',letterHandler, false)
+      divForLetters.appendChild(charBlock);
+    });
+  })() 
+};
